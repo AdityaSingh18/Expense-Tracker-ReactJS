5149265ffa59f3de3c55a14c78d3e5696ece55b9
Aditya <aditya18.mail@gmail.com>
2023-02-23 18:23:22 +0530

    Splitting Components into Smaller Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import ExpenseDate from './ExpenseDate'; 
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
+
 
 function ExpenseItem(props){
 
- 
-  const LocationOfExpenditure = 'Delhi'
     return (
         <div className='expense-item'>
-          <div>{props.date.toISOString()}</div>
+         <ExpenseDate date={props.date}/>
           <div>{props.LocationOfExpenditure}</div>
+
           <div className='expense-item_description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item_price'>{props.amount}</div>
+            <ExpenseDetails title={props.title} amount={props.amount}/>
           </div>
         </div>
       );
