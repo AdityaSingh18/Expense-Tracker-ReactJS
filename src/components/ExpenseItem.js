0838623c98d52121c4ba59dd62bfe60fb1858940
Aditya <aditya18.mail@gmail.com>
2023-02-23 12:24:24 +0530

    Adding ExpenseItem Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>
+          <div>Expense Tracker</div>
+          <div>
+            <h2>Food</h2>
+            <div>$294.67</div>
+          </div>
+        </div>
+      );
+}
+
+export default ExpenseItem;
